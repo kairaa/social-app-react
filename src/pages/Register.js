6686515fd87c036f16540a0e3d2bca742773bfaa
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2022-08-02 21:22:55 +0300

    Post in register without getting data from form inputs

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,72 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./Login.css";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 const Register = () => {
-  const clickHandler = () => {
-    console.log("clickd!!");
-  };
+  const [res, setRes] = useState([]);
+
+  // let user = {
+  //   userName: "kayra5",
+  //   password: "Aa7waw37x.",
+  //   firstName: "Kayra",
+  //   lastName: "Çakıroğlu",
+  //   email: "user@example.com",
+  // };
+
+  // const sendRequest = useCallback(async () => {
+  //   if (isSending) return;
+  //   setIsSending(true);
+  //   await axios
+  //     .post("https://localhost:7139/api/account/register", user)
+  //     .then((result) => {
+  //       setRes(result.data);
+  //     });
+  //   setIsSending(false);
+  //   console.log(res);
+  //   console.log(typeof res);
+  //   console.log(res.data);
+  //   console.log(res.status);
+  // });
+
+  async function sendRequest() {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        userName: "kayra9",
+        password: "Aa7waw37x.",
+        firstName: "Kayra",
+        lastName: "Çakıroğlu",
+        email: "user@example.com",
+      }),
+    };
+    const response = await fetch(
+      "https://localhost:7139/api/account/register",
+      requestOptions
+    );
+    if (response.status == 400) {
+      console.log(
+        "bişeyler eksik!! kendine çeki düzen ver öyle kaydını yapalım, piç"
+      );
+    }
+    console.log(response);
+  }
+
+  //DuplicateUserName
+  // const clickHandler = () => {
+  //   useEffect(() => {
+  //     axios
+  //       .post("https://localhost:7139/api/account/register", user)
+  //       .then((result) => {
+  //         setRes(result.data);
+  //       });
+  //   }, []);
+  // };
 
   return (
     <div className="container mt-5 mx-auto">
@@ -47,7 +108,7 @@ const Register = () => {
         <Button
           variant="primary"
           // type="submit"
-          onClick={clickHandler}
+          onClick={sendRequest}
         >
           Submit
         </Button>
